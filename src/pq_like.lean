c89941880b0d80a2fd53198aeb6ef5d6deccc42d
Torstein Vik <torsteinv64@gmail.com>
2021-03-24 14:22:18 +0100

    Added pq like cartesian isomorphism

--- src/pq_like.lean
+++ src/pq_like.lean
@@ -292,5 +292,48 @@ begin
 end
 
 
+def pq_group_prod_ker_G : pq_group (pq_group Q) ≃* pq_group Q × (counit : pq_group (pq_group Q) →* pq_group Q).ker := { 
+  to_fun := λ x, ⟨counit x, left_split_hom x⟩,
+  inv_fun := λ a, a.2 * (L_of_morph of of_is_pq_morphism a.1),
+  left_inv := begin 
+    intro x,
+    simp only,
+    unfold left_split_hom,
+    simp only [monoid_hom.coe_mk],
+    unfold left_split_fun,
+    simp only [subgroup.coe_mk, inv_mul_cancel_right],
+  end,
+  right_inv := begin 
+    intro x,
+    cases x with x1 x2,
+    simp only [monoid_hom.map_mul],
+    ext1,
+    {
+      simp only,
+      rw counit_L_of,
+      simp only [mul_left_eq_self],
+      cases x2 with x2 hx2,
+      simp only [subtype.coe_mk],
+      exact hx2,
+    },
+    {
+      simp only,
+      rw left_split_hom_is_left_split,
+      simp only [mul_right_eq_self],
+      unfold left_split_hom,
+      simp only [monoid_hom.coe_mk],
+      unfold left_split_fun,
+      simp_rw counit_L_of,
+      group,
+      refl,
+    },
+  end,
+  map_mul' := begin 
+    intros x y,
+    ext1,
+    simp only [monoid_hom.map_mul, prod.mk_mul_mk],
+    simp only [monoid_hom.map_mul, prod.mk_mul_mk],
+  end }
+
 end pq_like_split
 
